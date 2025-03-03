26d33a79e76d95552c5cb39688da9e4035f20b47
Nikhil Bhamare <itznikhil29@gmail.com>
2025-03-04 00:15:00 +0530

    feat: Added UI for not found page

--- src/components/not-found/NotFound.tsx
+++ src/components/not-found/NotFound.tsx
@@ -1,5 +1,30 @@
+import space from "../../assets/error-404.gif";
+import { Button } from "../ui/button";
+import { useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  return (
+    <motion.div
+      className="h-screen flex flex-col justify-center items-center text-center p-4"
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <img src={space} alt="Not Found" className="w-72 md:w-96" />
+      <h1 className="text-2xl md:text-3xl font-bold mt-4">
+        Oops! Page not found.
+      </h1>
+      <p className="text-gray-500 mt-2">
+        The page you're looking for doesn't exist.
+      </p>
+      <Button className="mt-4 cursor-pointer" onClick={() => navigate("/")}>
+        Go Home
+      </Button>
+    </motion.div>
+  );
 };
 
 export default NotFound;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,6 @@ const Home = () => {
   return (
     <div className="flex items-center justify-center">
       <Banner />
-      
     </div>
   );
 };
