ea0a222eb9354c34c77c31e0dbf0ad7227180353
Nikhil Bhamare <itznikhil29@gmail.com>
2025-04-17 16:18:01 +0530

    feat: Enhanced brand grid section responsiveness across devices

--- src/feature/featured-brands/FeaturedBrands.tsx
+++ src/feature/featured-brands/FeaturedBrands.tsx
@@ -3,7 +3,7 @@ import BrandGrid from "../../components/brand-grid/BrandGrid";
 
 const FeaturedBrands = () => {
   return (
-    <div className="flex-1 px-4 h-96">
+    <div className="flex-1 h-96 px-2 sm:px-4 ">
       <div className="flex items-center gap-2 bg-orange-500 rounded-md p-1 w-36">
         <Backpack className="text-white" />
         <h2 className="text-xl font-medium text-white">Top Brands</h2>
